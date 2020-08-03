997e61857887615cd75e6f5e536db74efc798f65
Naveen Krishna <naveen.yarlagadda@oneplus.com>
2020-08-03 12:43:05 +0530

    Add subsystem crash reason record and notify it by uevent
    Change-Id: I366e4336b76727718a09c58ce9b81827fed80933

--- drivers/soc/qcom/subsys-pil-tz.c
+++ drivers/soc/qcom/subsys-pil-tz.c
@@ -821,6 +821,7 @@ static void log_failure_reason(const struct pil_tz_data *d)
 						__builtin_return_address(0));
 	save_dump_reason_to_smem(reason, function_name);
 	pr_err("%s subsystem failure reason: %s.\n", name, reason);
+	subsys_store_crash_reason(d->subsys, reason);
 }
 
 static int subsys_shutdown(const struct subsys_desc *subsys, bool force_stop)
